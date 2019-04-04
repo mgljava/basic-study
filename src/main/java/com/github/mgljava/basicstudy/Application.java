64c69156b2026b748a29b0fee6f50ea76b02e068
Minggl <guangliang1120@gmail.com>
2019-04-04 18:09:46 +0800

    [TECH][Monk] add design pattern

--- src/main/java/com/github/mgljava/basicstudy/Application.java
+++ src/main/java/com/github/mgljava/basicstudy/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BasicStudyApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(BasicStudyApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
