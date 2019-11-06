a71e289b4ad73d0da076070dbe365da9b93ed102
Minggl <guangliang1120@gmail.com>
2019-11-06 15:04:38 +0800

    [TECH][Minggl]: 延时消息

---
+++ src/main/java/com/github/mgljava/basicstudy/rocketmq/consumer/DelayConsumer.java
@@ -0,0 +1,33 @@
+package com.github.mgljava.basicstudy.rocketmq.consumer;
+
+import java.util.List;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.message.MessageExt;
+
+public class DelayConsumer {
+
+  public static void main(String[] args) throws Exception {
+    // 实例化消费者
+    DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("DelayConsumer");
+    consumer.setNamesrvAddr("127.0.0.1:9876");
+    // 订阅Topics
+    consumer.subscribe("delayTestTopic", "*");
+    // 注册消息监听者
+    consumer.registerMessageListener(new MessageListenerConcurrently() {
+      @Override
+      public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> messages, ConsumeConcurrentlyContext context) {
+        for (MessageExt message : messages) {
+          // Print approximate delay time period
+          System.out.println(
+              "Receive message[msgId=" + message.getMsgId() + "] " + (System.currentTimeMillis() - message.getStoreTimestamp()) + "ms later");
+        }
+        return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+      }
+    });
+    // 启动消费者
+    consumer.start();
+  }
+}
---
+++ src/main/java/com/github/mgljava/basicstudy/rocketmq/producer/DelayProducer.java
@@ -0,0 +1,31 @@
+package com.github.mgljava.basicstudy.rocketmq.producer;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.Message;
+
+/**
+ * 延时消息
+ */
+public class DelayProducer {
+
+  public static void main(String[] args) throws Exception {
+    // 实例化一个生产者来产生延时消息
+    DefaultMQProducer producer = new DefaultMQProducer("DelayProducer");
+    // 启动生产者
+    producer.setNamesrvAddr("127.0.0.1:9876");
+    producer.start();
+    int totalMessagesToSend = 100;
+    for (int i = 0; i < totalMessagesToSend; i++) {
+      Message message = new Message("delayTestTopic", ("Hello scheduled message " + i).getBytes());
+      /*
+      设置延时等级3,这个消息将在10s之后发送(现在只支持固定的几个时间,详看delayTimeLevel).
+      messageDelayLevel = "1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m 30m 1h 2h"
+       */
+      message.setDelayTimeLevel(3);
+      // 发送消息
+      producer.send(message);
+    }
+    // 关闭生产者
+    producer.shutdown();
+  }
+}
