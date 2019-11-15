c0621f8973cd5d64fe881b562df513882b837c2e
Minggl <guangliang1120@gmail.com>
2019-11-15 21:49:12 +0800

    [TECH][Minggl]: implements code

--- src/main/java/com/github/mgljava/basicstudy/CaesarCipherTest.java
+++ src/main/java/com/github/mgljava/basicstudy/CaesarCipherTest.java
@@ -3,13 +3,17 @@ package com.github.mgljava.basicstudy;
 public class CaesarCipherTest {
 
   public static void main(String[] args) {
-    /*System.out.print(symbol('l', 1));
-    System.out.print(symbol('o', 1));
-    System.out.print(symbol('p', 1));
-    System.out.print(symbol('o', 1));
-    System.out.print(symbol('x', 1));*/
-    System.out.println(decode("ijj tipvme ibwf lopxo uibu zpv xpvme ibwf b qfsgfdu botxfs gps nf !!!", 1));
-    System.out.println(decode("opP DBQUBJO! nz Dbqubjo! pvs gfbsgvm usjq jt epof;", 1));
+    StringBuilder stringBuilder = new StringBuilder();
+    String[] code = {"ijJ tipvme ibw", "f lopxo uibu z", "pv xpvme ibwf ", "b qfsgfdu botx", "fs gps nf!!!"};
+    // String[] code = {"opP DBQUBJ", "O! nz Dbqu", "bjo! pvs g", "fbsgvm usj", "q jt epof;"};
+    for (String s : code) {
+      stringBuilder.append(s);
+    }
+    String result = stringBuilder.toString();
+    String substring = result.substring(0, result.indexOf(" "));
+    String wei = result.substring(result.indexOf(" "));
+    String decode = decode(wei, substring.charAt(1) - substring.charAt(0));
+    System.out.println(substring.charAt(0) + decode);
   }
 
   public static int getNum(char str) {
@@ -29,6 +33,8 @@ public class CaesarCipherTest {
   }
 
   public static char symbol(char ch, int shift) {
+    if (ch > 'z' || ch < 'A')
+      return ch;
     return getChar(getNum(ch) - shift);
   }
 }
