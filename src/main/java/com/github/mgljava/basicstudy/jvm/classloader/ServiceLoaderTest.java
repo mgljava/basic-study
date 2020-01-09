0a52de36ecea89ff08001766df2b54a7974845cf
Minggl <guangliang1120@gmail.com>
2020-01-09 11:09:27 +0800

    [TECH][Minggl]: add service classLoader class

--- src/main/java/com/github/mgljava/basicstudy/jvm/classloader/ServiceLoaderTest.java
+++ src/main/java/com/github/mgljava/basicstudy/jvm/classloader/ServiceLoaderTest.java
@@ -8,6 +8,13 @@ public class ServiceLoaderTest {
 
   public static void main(String[] args) {
 
+    // 如果加上此行代码，那么MySQL的驱动就无法加载，因为扩展类加载器无法加载类路径下的jar包
+    // Thread.currentThread().setContextClassLoader(ServiceLoaderTest.class.getClassLoader().getParent());
+
+    // 如果换成自定义类加载器，那么是可以加载的，因为自定义类加载器就是应用类加载器来加载的
+    System.out.println(MyClassLoader.class.getClassLoader()); // AppClassLoader
+    Thread.currentThread().setContextClassLoader(MyClassLoader.class.getClassLoader());
+
     ServiceLoader<Driver> loader = ServiceLoader.load(Driver.class);
     Iterator<Driver> iterator = loader.iterator();
     while (iterator.hasNext()) {
