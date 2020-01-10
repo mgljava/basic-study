80680bd8b136fc52869168ee18cc9f27d78c2f1b
Minggl <guangliang1120@gmail.com>
2020-01-10 14:52:52 +0800

    [TECH][Minggl]: refactor code

--- src/main/java/com/github/mgljava/basicstudy/jvm/classloader/InterfaceInitTest.java
+++ src/main/java/com/github/mgljava/basicstudy/jvm/classloader/InterfaceInitTest.java
@@ -6,7 +6,7 @@ import java.util.Random;
  * 如果引用的值是常量值，那么就不会加载接口，因为接口中定义的值时 public static final
  * 如果是类似于 Random 生成的值，那么就会加载接口
  */
-public class MyTest5 {
+public class InterfaceInitTest {
 
   public static void main(String[] args) {
     System.out.println(MyChild5.B);
