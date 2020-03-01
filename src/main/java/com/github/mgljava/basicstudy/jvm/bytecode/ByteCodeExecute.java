e0d767969e550f8173853ffcf56099b3a81498c1
Minggl <guangliang1120@gmail.com>
2020-03-01 16:18:10 +0800

    [TECH][Minggl]: 虚拟机字节码执行引擎

---
+++ src/main/java/com/github/mgljava/basicstudy/jvm/bytecode/ByteCodeExecute.java
@@ -0,0 +1,17 @@
+package com.github.mgljava.basicstudy.jvm.bytecode;
+
+/**
+ * 字节码执行引擎分析
+ */
+public class ByteCodeExecute {
+
+  public int myCalculate() {
+    int aa = -1;
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int d = 4;
+
+    return (a + b - c) * d;
+  }
+}
