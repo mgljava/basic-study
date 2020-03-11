e9b0fc91f344a07b57b55769b1b24110903a37ec
Minggl <guangliang1120@gmail.com>
2020-03-11 13:45:28 +0800

    [TECH][Minggl]: GC

--- src/main/java/com/github/mgljava/basicstudy/jvm/gc/MyTest3.java
+++ src/main/java/com/github/mgljava/basicstudy/jvm/gc/MyTest3.java
@@ -21,9 +21,9 @@ public class MyTest3 {
     int size = 1024 * 1024;
 
     byte[] myAlloc1 = new byte[2 * size];
-    byte[] myAlloc2 = new byte[3 * size];
-    byte[] myAlloc3 = new byte[3 * size];
-    byte[] myAlloc4 = new byte[3 * size];
+    byte[] myAlloc2 = new byte[2 * size];
+    byte[] myAlloc3 = new byte[2 * size];
+    byte[] myAlloc4 = new byte[2 * size];
     System.out.println("Hello World!");
   }
 }
