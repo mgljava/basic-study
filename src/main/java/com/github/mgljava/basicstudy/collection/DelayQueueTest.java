58126026ae225dde992658d08962d2b716b081a9
Minggl <guangliang1120@gmail.com>
2020-11-10 13:53:00 +0800

    [TECH][Minggl]: code

--- src/main/java/com/github/mgljava/basicstudy/collection/DelayQueueTest.java
+++ src/main/java/com/github/mgljava/basicstudy/collection/DelayQueueTest.java
@@ -6,21 +6,52 @@ import java.util.concurrent.TimeUnit;
 
 public class DelayQueueTest {
 
-  public static void main(String[] args) {
-    DelayQueue<MyDelayed> queue = new DelayQueue<>();
-    queue.add(new MyDelayed());
+  public static void main(String[] args) throws InterruptedException {
+    final DelayQueue<MyDelayed> delayQueue = new DelayQueue<>();
+    new Thread(() -> {
+      delayQueue.offer(new MyDelayed("task1", 10000));
+      delayQueue.offer(new MyDelayed("task2", 3900));
+      delayQueue.offer(new MyDelayed("task3", 1900));
+      delayQueue.offer(new MyDelayed("task4", 5900));
+      delayQueue.offer(new MyDelayed("task5", 6900));
+      delayQueue.offer(new MyDelayed("task6", 7900));
+      delayQueue.offer(new MyDelayed("task7", 4900));
+    }).start();
+
+    while (true) {
+      Delayed take = delayQueue.take();
+      System.out.println(take);
+    }
   }
 }
 
 class MyDelayed implements Delayed {
 
+  private final String name;
+  private final long start = System.currentTimeMillis();
+  private final long time;
+
+  public MyDelayed(String name, long time) {
+    this.name = name;
+    this.time = time;
+  }
+
   @Override
   public long getDelay(TimeUnit unit) {
-    return 0;
+    return unit.convert((start + time) - System.currentTimeMillis(), TimeUnit.MILLISECONDS);
   }
 
   @Override
   public int compareTo(Delayed o) {
-    return 0;
+    MyDelayed myDelayed = (MyDelayed) o;
+    return (int) (this.getDelay(TimeUnit.MILLISECONDS) - myDelayed.getDelay(TimeUnit.MILLISECONDS));
+  }
+
+  @Override
+  public String toString() {
+    return "MyDelayedTask{" +
+        "name='" + name + '\'' +
+        ", time=" + time +
+        '}';
   }
 }
