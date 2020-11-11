2fdc6aa7e2daf0707da1efc01cbf5bd50f80aa53
Minggl <guangliang1120@gmail.com>
2020-11-11 17:32:39 +0800

    [TECH][Minggl]: code

--- src/main/java/com/github/mgljava/basicstudy/leetcode/middle/BoundedBlockingQueue.java
+++ src/main/java/com/github/mgljava/basicstudy/leetcode/middle/BoundedBlockingQueue.java
@@ -1,48 +1,66 @@
 package com.github.mgljava.basicstudy.leetcode.middle;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BoundedBlockingQueue {
 
-  public static void main(String[] args) throws Exception {
-    BoundedBlockingQueue boundedBlockingQueue = new BoundedBlockingQueue(3);
-    boundedBlockingQueue.enqueue(2);
-    boundedBlockingQueue.enqueue(3);
-    boundedBlockingQueue.enqueue(4);
-    System.out.println("size: " + boundedBlockingQueue.size());
-    System.out.println(boundedBlockingQueue.dequeue());
-    System.out.println(boundedBlockingQueue.dequeue());
-    System.out.println(boundedBlockingQueue.dequeue());
-  }
-
-  int[] item;
+  int[] items;
   int count;
-  int takeIndex;
   int putIndex;
+  int takeIndex;
+  ReentrantLock lock = new ReentrantLock();
+  Condition takeLock = lock.newCondition();
+  Condition putLock = lock.newCondition();
 
   public BoundedBlockingQueue(int capacity) {
-    item = new int[capacity];
+    items = new int[capacity];
   }
 
   public void enqueue(int element) throws InterruptedException {
-    int[] items = this.item;
-    items[putIndex] = element;
-    if (++putIndex == items.length) {
-      putIndex = 0;
+    lock.lock();
+    try {
+      while (count == items.length) {
+        putLock.await();
+      }
+      items[putIndex++] = element;
+      count++;
+      takeLock.signal();
+    } finally {
+      lock.unlock();
     }
-    count++;
   }
 
   public int dequeue() throws InterruptedException {
-    int[] items = this.item;
-    int ans = items[takeIndex];
-    items[takeIndex] = 0;
-    if (++takeIndex == items.length) {
-      takeIndex = 0;
+    lock.lock();
+    try {
+      while (count == 0) {
+        takeLock.await();
+      }
+      int item = items[takeIndex++];
+      putLock.signal();
+      count--;
+      return item;
+    } finally {
+      lock.unlock();
     }
-    count--;
-    return ans;
   }
 
   public int size() {
     return count;
+  }
+
+  public static void main(String[] args) throws Exception {
+    BoundedBlockingQueue queue = new BoundedBlockingQueue(2);   // 使用capacity = 2初始化队列。
+
+    queue.enqueue(1);   // 生产者线程将1插入队列。
+    System.out.println(queue.dequeue()); // 消费者线程调用dequeue并返回1。
+    queue.dequeue();    // 由于队列为空，消费者线程被阻塞。
+    queue.enqueue(0);   // 生产者线程将0插入队列。消费者线程被解除阻塞同时将0弹出队列并返回。
+    queue.enqueue(2);   // 生产者线程将2插入队列。
+    queue.enqueue(3);   // 生产者线程将3插入队列。
+    queue.enqueue(4);   // 生产者线程由于队列长度已达到上限2而被阻塞。
+    queue.dequeue();    // 消费者线程将2从队列弹出并返回。生产者线程解除阻塞同时将4插入队列。
+    System.out.println(queue.size());       // 队列中还有2个元素。size()方法在每组测试用例最后调用。
   }
 }
