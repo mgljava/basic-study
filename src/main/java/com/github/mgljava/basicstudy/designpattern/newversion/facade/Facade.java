15152a3ad55872058d6885e2fa8242cde452c0c7
Minggl <guangliang1120@gmail.com>
2021-04-06 18:21:36 +0800

    [TECH][Minggl]: 门面模式

--- src/main/java/com/github/mgljava/basicstudy/designpattern/newversion/facade/Facade.java
+++ src/main/java/com/github/mgljava/basicstudy/designpattern/newversion/facade/Facade.java
@@ -2,19 +2,21 @@ package com.github.mgljava.basicstudy.designpattern.newversion.facade;
 
 public class Facade {
 
-  private ServiceA serviceA;
-  private ServiceB serviceB;
+  private final ServiceA serviceA;
+  private final ServiceB serviceB;
 
   public Facade() {
     serviceA = new ServiceAImpl();
     serviceB = new ServiceBImpl();
   }
 
+  // 门面方法1，可以任意组装调用，客户端不必知道由哪些方法组成的
   public void methodA() {
     serviceA.serviceA();
     serviceB.serviceB();
   }
 
+  // 门面方法2
   public void methodB() {
     serviceB.serviceB();
     serviceA.serviceA();
