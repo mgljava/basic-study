d1e736ea2a9b36e1e9cc1aadd5a2a1a4a75d9121
Minggl <guangliang1120@gmail.com>
2021-04-09 09:40:46 +0800

    [TECH][Minggl]: 备忘录模式

---
+++ src/main/java/com/github/mgljava/basicstudy/designpattern/newversion/memento/MainTest.java
@@ -0,0 +1,19 @@
+package com.github.mgljava.basicstudy.designpattern.newversion.memento;
+
+public class MainTest {
+
+  public static void main(String[] args) {
+    Person person = new Person("张三", "男", 22);
+    Caretaker caretaker = new Caretaker();
+    caretaker.setMemento(person.createMemento());
+    System.out.println(person.toString());
+
+    // 进行修改
+    person.setName("李四");
+    System.out.println(person.toString());
+
+    // 回复到原来的状态
+    person.setMemento(caretaker.getMemento());
+    System.out.println(person.toString());
+  }
+}
---
+++ src/main/java/com/github/mgljava/basicstudy/designpattern/newversion/memento/Person.java
@@ -0,0 +1,61 @@
+package com.github.mgljava.basicstudy.designpattern.newversion.memento;
+
+// 人员类，需要后面被恢复的对象
+public class Person {
+
+  private String name;
+  private String sex;
+  private int age;
+
+  public Person() {
+  }
+
+  public Person(String name, String sex, int age) {
+    this.name = name;
+    this.sex = sex;
+    this.age = age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getSex() {
+    return sex;
+  }
+
+  public void setSex(String sex) {
+    this.sex = sex;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  @Override
+  public String toString() {
+    return "Person{" +
+        "name='" + name + '\'' +
+        ", sex='" + sex + '\'' +
+        ", age=" + age +
+        '}';
+  }
+
+  public Memento createMemento() {
+    return new Memento(name, sex, age);
+  }
+
+  public void setMemento(Memento memento) {
+    this.name = memento.getName();
+    this.age = memento.getAge();
+    this.sex = memento.getSex();
+  }
+}
